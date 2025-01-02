8d4dba03a9eb6511739d0c364710ee949514d047
Alexey Sundyrev <aleksey.sundyrev@gmail.com>
2025-01-02 17:32:58 +0000

    Make the project name and path required

--- delete.sh
+++ delete.sh
@@ -1,32 +1,44 @@
 #!/bin/bash
-service_name="nowknow"
-project_path="/home/alexey/projects/nowknowApp"
 
-read -p "Are you sure you want to delete the project $project_path? [y/N]: " confirm
+read -p "Project name: " project_name
+read -p "Project path: " project_path
+if [[ -z "$project_path" ]]; then
+    echo "Error: Project path is required."
+    exit 1
+fi
+
+read -p "Are you sure you want to delete the project at $project_path? [y/N]: " confirm
 if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
     echo "Deletion canceled."
     exit 0
 fi
 
+# Check if the project path matches the script's directory
+script_dir=$(dirname "$(realpath "$0")")
+if [[ "$project_path" == "$script_dir" ]]; then
+    echo "Error: Cannot delete the script's directory ($script_dir)."
+    exit 1
+fi
+
 echo "Stopping the Gunicorn socket..."
-sudo systemctl stop ${service_name}.gunicorn.socket &>/dev/null
+sudo systemctl stop ${project_name}.gunicorn.socket &>/dev/null
 
 echo "Stopping the Gunicorn service..."
-sudo systemctl stop ${service_name}.gunicorn.service &>/dev/null
+sudo systemctl stop ${project_name}.gunicorn.service &>/dev/null
 
 echo "Disabling the Gunicorn service and socket..."
-sudo systemctl disable ${service_name}.gunicorn.service &>/dev/null
-sudo systemctl disable ${service_name}.gunicorn.socket &>/dev/null
+sudo systemctl disable ${project_name}.gunicorn.service &>/dev/null
+sudo systemctl disable ${project_name}.gunicorn.socket &>/dev/null
 
 echo "Removing symbolic links for the Gunicorn service and socket..."
-sudo find /etc/systemd/system -type l -name "${service_name}.gunicorn.*" -delete
+sudo find /etc/systemd/system -type l -name "${project_name}.gunicorn.*" -delete
 
 echo "Reloading systemd to refresh the configuration..."
 sudo systemctl daemon-reload &>/dev/null
 
 echo "Removing Nginx configuration..."
-if [[ -f "/etc/nginx/sites-enabled/${service_name}.nginx.conf" ]]; then
-    sudo rm "/etc/nginx/sites-enabled/${service_name}.nginx.conf" &>/dev/null
+if [[ -f "/etc/nginx/sites-enabled/${project_name}.nginx.conf" ]]; then
+    sudo rm "/etc/nginx/sites-enabled/${project_name}.nginx.conf" &>/dev/null
     sudo systemctl reload nginx &>/dev/null
 fi
 
