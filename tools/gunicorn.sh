b0233412e73c8807db1aab45afcaba930a1cb869
Aleksey Sundyrev <aleksey.sundyrev@gmail.com>
2025-04-27 20:22:59 +0300

    feat: add environment variable support for tools scripts

--- tools/gunicorn.sh
+++ tools/gunicorn.sh
@@ -1,15 +1,55 @@
 #!/bin/bash
 set -e
 
-read -e -p "Project name: " project_name
-if [[ -z "${project_name}" ]]; then
-    echo -e "\e[38;5;196m[ERROR]\e[0m Project name is required."
+# Function to display usage information
+usage() {
+    echo "Usage: $0 [--project PROJECT_NAME]"
+    echo "  --project  Project name. If not specified, it will be prompted from the user."
     exit 1
+}
+
+# Parse command-line parameters
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --project)
+            if [[ -n "$2" && "$2" != --* ]]; then
+                project_name="$2"
+                shift 2
+            else
+                echo -e "\e[38;5;196m[ERROR]\e[0m The --project parameter requires a value."
+                usage
+            fi
+            ;;
+        *)
+            echo -e "\e[38;5;196m[ERROR]\e[0m Unknown parameter: $1"
+            usage
+            ;;
+    esac
+done
+
+# If project_name is not specified, prompt the user
+if [[ -z "$project_name" ]]; then
+    read -e -p "Project name: " project_name
+    if [[ -z "${project_name}" ]]; then
+        echo -e "\e[38;5;196m[ERROR]\e[0m Project name is required."
+        exit 1
+    fi
 fi
 
-read -e -p "Project path: " project_path
-project_path="${project_path/#\~/$HOME}"
-project_path=$(realpath -m "${project_path}")
+# Check for project path in environment variable or prompt the user
+project_path_var="${project_name^^}_PATH"
+project_path="${!project_path_var}"
+
+# Expand ~ in project_path if it comes from environment variable
+if [[ -n "$project_path" ]]; then
+    project_path="${project_path/#\~/$HOME}"
+fi
+
+if [[ -z "$project_path" ]]; then
+    read -e -p "Project path: " project_path
+    project_path="${project_path/#\~/$HOME}"
+    project_path=$(realpath -m "${project_path}")
+fi
 
 if [[ -z "${project_path}" ]]; then
     echo -e "\e[38;5;196m[ERROR]\e[0m Project path is required."
